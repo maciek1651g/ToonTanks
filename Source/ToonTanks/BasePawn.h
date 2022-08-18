01edbb84431f47db78dc48e59d65cd9b750f730d
Maciej Dominiak <maciek1651g@gmail.com>
2022-08-18 21:03:49 +0200

    defaults and instances read/write

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 Health = 100;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
